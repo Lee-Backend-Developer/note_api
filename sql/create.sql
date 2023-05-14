8befb60250cdfbe852e6de09261afef5e2e2dcb4
Lee-Backend-Developer <eotmd1016@gmail.com>
2023-05-14 22:47:06 +0900

    서버 시작할 때 마다 초기값들을 넣어줌

---
+++ sql/create.sql
@@ -0,0 +1,4 @@
+insert into member(member_id, login_id, password) values(1, 'test', '1234');
+insert into category(category_id, name, member_member_id) values(1, '카테고리1', 1);
+insert into note(note_id, content, category_category_id, member_member_id) values(1, '콘텐츠1', 1, 1);
+insert into note(note_id, content, category_category_id, member_member_id) values(2, '콘텐츠2', 1, 1);
