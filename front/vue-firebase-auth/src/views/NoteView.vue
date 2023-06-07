e41f78361321a558897ca1bec9ff49f97ee1d4b0
Lee-Backend-Developer <eotmd1016@gmail.com>
2023-06-07 09:17:25 +0900

    Note 화면에 note들이 밑으로 쏠리는 현상 해결하였음, 사용하지 않는 css 정리

--- front/vue-firebase-auth/src/views/NoteView.vue
+++ front/vue-firebase-auth/src/views/NoteView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, ref } from "vue";
+import {onMounted, ref} from "vue";
 import axios from "axios";
 import router from "@/router";
 
@@ -12,40 +12,40 @@ let categoryId;
 
 
 onMounted(() => {
-  getNotes();
-  categoryGet();
+    getNotes();
+    categoryGet();
 });
 
 function getNotes() {
-  const url = "/api/note";
-  axios
-    .get(url)
-    .then(function (response) {
-      for (let obj of response.data) {
-        let noteObj = {
-          id: obj.id,
-          content: obj.content,
-          category: obj.category,
-        };
-        notes.value.push(noteObj);
-      }
-    })
-    .catch(function (error) {
-      console.log(error);
-      let response = error.response.data;
-      if (response.code == 400) {
-        console.log(response.message);
-        alert(response.message);
-      }
-      console.log(error);
-    });
+    const url = "/api/note";
+    axios
+        .get(url)
+        .then(function (response) {
+            for (let obj of response.data) {
+                let noteObj = {
+                    id: obj.id,
+                    content: obj.content,
+                    category: obj.category,
+                };
+                notes.value.push(noteObj);
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+            let response = error.response.data;
+            if (response.code == 400) {
+                console.log(response.message);
+                alert(response.message);
+            }
+            console.log(error);
+        });
 }
 
-function categoryGet(){
+function categoryGet() {
     axios.get("/api/category")
         .then(function (response) {
             let data = response.data;
-            for(let category of data) {
+            for (let category of data) {
                 let categoryObj = {
                     categoryId: category.categoryId,
                     name: category.name
@@ -56,183 +56,157 @@ function categoryGet(){
 }
 
 const addNote = () => {
-  if (newNote.value.length < 10) {
-    return (errorMessage.value = "Note must be at least 10 characters long!");
-  }
+    if (newNote.value.length < 10) {
+        return (errorMessage.value = "Note must be at least 10 characters long!");
+    }
 
-  const url = "/api/note/write";
-  const request = {
-    content: newNote.value,
-    categoryId: categoryId,
-  };
-  axios
-    .post(url, request)
-    .then(function (response) {
-      console.log("메모를 성공적으로 등록됨", response);
-      router.go(0);
-    })
-    .catch(function (error) {
-      console.log("메모를 전송 실패", error);
-    });
-  showModal.value = false;
-  newNote.value = "";
-  errorMessage.value = "";
+    const url = "/api/note/write";
+    const request = {
+        content: newNote.value,
+        categoryId: categoryId,
+    };
+    axios
+        .post(url, request)
+        .then(function (response) {
+            console.log("메모를 성공적으로 등록됨", response);
+            router.go(0);
+        })
+        .catch(function (error) {
+            console.log("메모를 전송 실패", error);
+        });
+    showModal.value = false;
+    newNote.value = "";
+    errorMessage.value = "";
 };
+
 function categoryChange(event) {
     categoryId = event.target.value;
 }
 </script>
 <template>
-  <main>
-    <div v-if="showModal" class="overlay">
-      <div class="modal">
-          <select class="form-select" aria-label="Default select example" @change="categoryChange($event)">
-              <option selected>카테고리 선택</option>
-              <option v-for="category in categorios"
-                      :key="category.categoryId"
-                      :value="category.categoryId">
-                  {{ category.name }}
-              </option>
-          </select>
-        <textarea
-          v-model.trim="newNote"
-          name="note"
-          id="note"
-          cols="30"
-          rows="10"
-        ></textarea>
-        <p v-if="errorMessage">{{ errorMessage }}</p>
-        <button @click="addNote">Add Note</button>
-        <button class="cancel" @click="showModal = false">Cancel</button>
-      </div>
-    </div>
-    <div class="container">
-      <header>
-        <h1>Notes</h1>
-        <button @click="showModal = true">+</button>
-      </header>
-
-      <div class="cards-container">
-        <div
-          v-for="note in notes"
-          :key="note.id"
-          class="card"
-          :style="{ backgroundColor: note.backgroundColor }"
-        >
-          <p class="main-text">{{ note.content }}</p>
+    <main>
+        <div v-if="showModal" class="overlay">
+            <div class="modal">
+                <select class="form-select" aria-label="Default select example" @change="categoryChange($event)">
+                    <option selected>카테고리 선택</option>
+                    <option v-for="category in categorios"
+                            :key="category.categoryId"
+                            :value="category.categoryId">
+                        {{ category.name }}
+                    </option>
+                </select>
+                <textarea
+                        v-model.trim="newNote"
+                        name="note"
+                        id="note"
+                        cols="30"
+                        rows="10"
+                ></textarea>
+                <p v-if="errorMessage">{{ errorMessage }}</p>
+                <button @click="addNote">Add Note</button>
+                <button class="cancel" @click="showModal = false">Cancel</button>
+            </div>
         </div>
-      </div>
-    </div>
-  </main>
+        <div class="container-fluid">
+            <header>
+                <h1>Notes</h1>
+                <button @click="showModal = true">+</button>
+            </header>
+    <!-- todo note 순서가 밑으로 쏠리는 현상을 해결해야됨-->
+            <div class="container-fluid">
+                <div class="row row-cols-auto">
+                    <div class="col" v-for="note in notes" :key="note.id">
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body">
+                                <h5 class="card-title">category: {{ note.category }}</h5>
+                                <p class="card-text">{{ note.content }}</p>
+                                <a href="#" class="btn btn-primary m-2">삭제</a>
+                                <a href="#" class="btn btn-primary">수정</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
 
 <style scoped>
 main {
-  height: 100vh;
-  width: 100vw;
-  top: 58% !important;
-}
-
-.container {
-  max-width: 1000px;
-  padding: 10px;
-  margin: 0 auto;
-  position: relative;
+    height: 100vh;
+    width: 100vw;
+    top: 58% !important;
 }
 
 header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 
 h1 {
-  font-weight: bold;
-  margin-bottom: 25px;
-  font-size: 75px;
+    font-weight: bold;
+    margin-bottom: 25px;
+    font-size: 75px;
+
 }
 
 button {
-  border: none;
-  padding: 10px;
-  width: 50px;
-  height: 50px;
-  cursor: pointer;
-  background-color: black;
-  border-radius: 100%;
-  color: white;
-  font-size: 20px;
-}
-
-.card {
-  width: 225px;
-  height: 100%;
-  min-height: 225px;
-  background-color: rgb(237, 182, 44);
-  padding: 20px;
-  gap: 10px;
-  border-radius: 2px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  margin-right: 20px;
-  margin-bottom: 20px;
-  word-wrap: break-word;
-}
-
-.date {
-  font-size: 12px;
-  font-weight: bold;
-}
-
-.cards-container {
-  display: flex;
-  flex-wrap: wrap;
+    border: none;
+    padding: 10px;
+    width: 50px;
+    height: 50px;
+    cursor: pointer;
+    background-color: black;
+    border-radius: 100%;
+    color: white;
+    font-size: 20px;
+    margin-left: 40px;
 }
 
 .overlay {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  z-index: 10;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 10;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 
 .modal {
-  width: 750px;
-  background-color: white;
-  border-radius: 2px;
-  padding: 20px;
-  position: relative;
-  display: flex;
-  flex-direction: column;
+    width: 750px;
+    background-color: white;
+    border-radius: 2px;
+    padding: 20px;
+    position: relative;
+    display: flex;
+    flex-direction: column;
 }
 
 .modal button {
-  padding: 10px 20px;
-  font-size: 20px;
-  width: 100%;
-  background-color: black;
-  border: none;
-  border-radius: 2px;
-  color: white;
-  cursor: pointer;
-  margin-top: 15px;
+    padding: 10px 20px;
+    font-size: 20px;
+    width: 100%;
+    background-color: black;
+    border: none;
+    border-radius: 2px;
+    color: white;
+    cursor: pointer;
+    margin-top: 15px;
 }
 
 .modal .cancel {
-  background-color: white;
-  color: black;
-  border: 2px solid black;
-  margin-top: 8px;
+    background-color: white;
+    color: black;
+    border: 2px solid black;
+    margin-top: 8px;
 }
 
 .modal p {
-  color: darkred;
-  margin-top: 8px;
+    color: darkred;
+    margin-top: 8px;
 }
 
 </style>
